ed1501871126ca2e84e42293327d94d2a0227dcb
Oleh Mushka <olegamysk@gmail.com>
2024-05-24 14:40:03 +0300

    IIE-6 updated id generator

--- fxidgenerator/module.go
+++ fxidgenerator/module.go
@@ -1,33 +1,20 @@
 package fxidgenerator
 
 import (
-	"fmt"
-
 	idgenerator "github.com/ecumenos-social/id-generator"
 	"go.uber.org/fx"
 )
 
 type Config struct {
-	TopLevelNode int64
-	LowLevelNode int64
-}
-
-func (cfg *Config) Validate() error {
-	if maxTopLevelNodeValue := int64(1<<idgenerator.TopLevelMachineBits - 1); cfg.TopLevelNode < 0 || cfg.TopLevelNode > maxTopLevelNodeValue {
-		return fmt.Errorf("invalid top level node (requirements: 0 < top_level_node <= %d, actual: %d)", maxTopLevelNodeValue, cfg.TopLevelNode)
-	}
-	if maxLowLevelNodeValue := int64(1<<idgenerator.LowLevelMachineBits - 1); cfg.LowLevelNode < 0 || cfg.LowLevelNode > maxLowLevelNodeValue {
-		return fmt.Errorf("invalid top level node (requirements: 0 < top_level_node <= %d, actual: %d)", maxLowLevelNodeValue, cfg.LowLevelNode)
-	}
-	return nil
+	TopNodeID int64
+	LowNodeID int64
 }
 
 var Module = fx.Options(
 	fx.Provide(func(config *Config) (idgenerator.Generator, error) {
-		if err := config.Validate(); err != nil {
-			return nil, err
-		}
-
-		return idgenerator.New(config.LowLevelNode, config.TopLevelNode)
+		return idgenerator.New(&idgenerator.NodeID{
+			Top: config.TopNodeID,
+			Low: config.LowNodeID,
+		})
 	}),
 )
